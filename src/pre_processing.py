48a95f150c3d4fab3f4c5e400a61fc9ebc9bda54
Shams <skm478@gmail.com>
2024-02-05 14:48:25 -0500

    prepro v2

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -20,8 +20,121 @@ save_path = '../NYC_Edison/Processed/'
 save_folder = '/home/shams/Development/code/post-grad/Edison-NYC-Bot/NYC_Edison/Processed'
 #Pre-Processing Test
 
+def all_text_black(img):
+    hsv_im = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+    # lower_thresh = np.array([90,50,50])
+    # upper_thresh = np.array([130,255,255])
+
+    lower_thresh = np.array([100,100,150])
+    upper_thresh = np.array([110,255,255])
+
+    blue_mask = cv.inRange(hsv_im, lower_thresh, upper_thresh) #Same image but areas in range are black (0) and areas that arent are white(255)
+
+    _, thresh = cv.threshold(blue_mask, 127, 255, cv.THRESH_BINARY) #tuple 0 is threshold value 1 is the image arr
+    cnts,_ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    potential_matches = []
+    for c in cnts:
+        x, y, w, h = cv.boundingRect(c)
+        
+        if w> 5*h:
+            potential_matches.append(c)
+            # cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0),2)
+            
+            for i in range(x,x+w):
+                for j in range(y,y+h):
+                    #Check if pixel within the bounding box is within the contour
+                    dist = cv.pointPolygonTest(c, (i,j), measureDist=False)
+                    # if dist > 0: HSV
+                    #     #pixel mod
+                    #     hue = hsv_im[j,i,0]
+                    #     value = hsv_im[j,i,2]
+
+                    #     if value > 200: #pixel is white
+                    #         hsv_im[j,i,2] = 0 #pixel is now black
+                    #     elif 100<=hue<=110: #pixel is blue
+                    #         hsv_im[j,i,0] = 0 #pixel is white
+
+                    if dist > 0: #for BGR
+                        # Access the pixel value in BGR format
+                        blue = img[j, i, 0]  # Blue channel
+                        green = img[j, i, 1]  # Green channel
+                        red = img[j, i, 2]  # Red channel
+
+                        # Invert the colors based on your criteria
+                        if blue > 130 and green > 130 and red > 130:  # Assuming white pixels have high intensity in all channels
+                            img[j, i] = (0, 0, 0)  # Set pixel to black
+                        elif blue >= 90 and blue <= 130 and green < 50 and red < 50:  # Assuming blue pixels have high intensity in blue channel and low intensity in green and red channels
+                            img[j, i] = (255, 255, 255)  # Set pixel to white
+    
+    cv.imwrite('jajaja.jpg', img)
+    return img
+
+    # for c in cnts:
+    #     rect = cv.minAreaRect(c)    
+    #     if rect[1][0] >= 5* rect[1][1]:
+    #         #Lets look at this contour as an area to invert
+    #         box = cv.boxPoints(rect)
+    #         box = np.int0(box)
+
+    #         #iterate over the pixels within the box shape
+    #         for i in range(box.shape[0]):
+    #             dist = cv.pointPolygonTest(c, i, measureDist=False)
+    #             if dist >0:
+    #                 print('got a hit')
+
+def find_textBlock_gu(img):
+    #initial gaussian blur 5x5 to get the contour == for the atable of interest
+    pass
 
 
+def find_textBlock(img):
+
+    #Determine Image Type. If it is primarily Black and White aka GC image what we have right now is fine for extracting
+
+    #GU takes images from computers whch has a few issues. 
+    #HSV value for the blue is 206-100-73 
+
+    #find area of image
+
+    area_original = img.shape[0] * img.shape[1] #wigth*height
+
+    #prepare image for contouring
+    
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray, (7,7), 0) #Is thisnecessary?
+    _, thresh = cv.threshold(blur, 127, 255, cv.THRESH_BINARY_INV) #tuple 0 is threshold value 1 is the image arr
+    
+    # Erode the Image to combine all the text into chunks not sure how necessary this will be. 
+    kernel_7 = np.ones((7,7))
+
+
+
+    dilation_image = cv.dilate(thresh, kernel_7, iterations=3) 
+    cv.imwrite('dilation_image.jpg', dilation_image)
+    #keep iterating until we get a good result?
+    
+    #Erotion is expanding black areas
+    #Dilation is expanding white areas areas
+
+
+    #Find the contours
+    #Contours in opencv is looking for white on black so inv thresholding is better. 
+    cnts,_ = cv.findContours(dilation_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # cv.drawContours(img, cnts, -1, (0,255,0), 3) #to visualize atm
+
+    #find the bounding boxes for the contours, select bounding box where area >20% of picture area, and less that < 90% 
+    for c in cnts:
+        x, y, w, h = cv.boundingRect(c)
+        area = w*h
+        if  (.2 * area_original) < area < (.9 * area_original): 
+            cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0),2)
+
+    #Use the bounding boxes to define my ROI which is the text block. 
+    #Pixel density? Largest Areas? 
+    
+    cv.imwrite('jajaja_textblocks.jpg', img)
+    pass   
 
 def invert(path, save_path=save_folder):
     edit_img = cv.imread(path)
@@ -48,10 +161,6 @@ def resize(img, save_path=save_folder, name='resize',date=None):
     resized = cv.resize(img,None,fx=2,fy=2 ,interpolation=cv.INTER_CUBIC ) 
     cv.imwrite(f'{save_path}/{name}_{date}.jpg', resized)
     return resized
-
-gu = '/home/shams/Development/code/post-grad/Edison-NYC-Bot/NYC_Edison/gu_test.jpg'
-
-resized = '/home/shams/Development/code/post-grad/Edison-NYC-Bot/NYC_Edison/Processed/resize_01-22-2024.jpg'
 
 ##Thresholding and Binarization
 
@@ -87,7 +196,6 @@ def deskew(img, save_path=save_folder, name='straightened', date = None):
     #We perform the rotation and save the new image.
         
     grey_img = grey(img)
-
     edges = cv.Canny(grey_img, 100 , 100, apertureSize=3)    
 
     plt.subplot(121),plt.imshow(img,cmap = 'gray')
@@ -116,9 +224,6 @@ def deskew(img, save_path=save_folder, name='straightened', date = None):
 
 
 
-im_path = '/home/shams/Development/code/post-grad/Edison-NYC-Bot/NYC_Edison/gu_1_22_24_crop.jpg'
-
-img = cv.imread(im_path)
 
 #Sharpening
 #look into kernals and images
